74da2dd0ad91ddd566c15e029e74bf745446b18a
Dennys Barros <51927122+barrosdennys@users.noreply.github.com>
2020-03-19 17:39:01 -0300

    Update CompleteTasksAssignedToUsers.groovy

--- completeTasks/CompleteTasksAssignedToUsers.groovy
+++ completeTasks/CompleteTasksAssignedToUsers.groovy
@@ -1,5 +1,6 @@
 //Complete ALL open tasks from all users 
 
+import com.liferay.petra.string.*
 import com.liferay.portal.kernel.dao.orm.*
 import com.liferay.portal.kernel.model.*
 import com.liferay.portal.kernel.service.*
@@ -9,14 +10,12 @@ import com.liferay.portal.kernel.workflow.*
 import com.liferay.blogs.service.*
 import java.util.*
 
-idCount = com.liferay.portal.kernel.service.UserLocalServiceUtil.getUsersCount() - 2;
 companyId = com.liferay.portal.kernel.util.PortalUtil.getCompanyId(actionRequest)
-mainUserId = com.liferay.portal.kernel.util.PortalUtil.getUserId(actionRequest)
 List<User> users = com.liferay.portal.kernel.service.UserLocalServiceUtil.getUsers(0,50);
 List<User> activeUsers = new ArrayList<User>();
 
 for (User user : users){
-    if (user.getEmailAddress().contains("test")) {
+    if (user.getEmailAddress().contains("user")) {
         activeUsers.add(user);
     }
 }
